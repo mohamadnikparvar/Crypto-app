73d272ee82023a4cbe4fa1426563a44b1fe6f34c
mohamadnikparvar <88265337+mohamadnikparvar@users.noreply.github.com>
2021-11-14 13:52:22 +0330

    data catched fro API in api.js

---
+++ crypto-app/src/components/Landing.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+
+// API
+import { getCoin } from '../services/api';
+
+const Landing = () => {
+
+    useEffect(() =>{
+        const fetchAPI =async () =>{
+            const data = await getCoin()
+            console.log(data)
+        }
+        fetchAPI()
+    },[])
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Landing;
