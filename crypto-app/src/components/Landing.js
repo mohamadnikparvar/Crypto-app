dee11dd76de3699bdf3ec9cd555bb5f53aff0fcf
mohamadnikparvar <88265337+mohamadnikparvar@users.noreply.github.com>
2021-11-14 15:28:49 +0330

    Landing and Loader components added to my project

--- crypto-app/src/components/Landing.js
+++ crypto-app/src/components/Landing.js
@@ -1,22 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 // API
 import { getCoin } from '../services/api';
 
+// components
+import Loader from './Loader';
+
 const Landing = () => {
+
+    const [coins,setCoins] = useState([])
 
     useEffect(() =>{
         const fetchAPI =async () =>{
             const data = await getCoin()
-            console.log(data)
+            setCoins(data)
         }
         fetchAPI()
     },[])
 
     return (
+        <>
+        <input type="text" placeholder="Search"></input>
+        {
+            coins.length?
         <div>
-            
-        </div>
+            {
+                coins.map(coin => <p key={coin.id}>{coin.name}</p>)
+            }
+        </div> :
+        <Loader/>   
+        }
+        
+        </>
     );
 };
 
